97156ed0082eff81d6e213a5922f1ca59359d401
Jennifer Wilcox <jennifer@nitori.org>
2019-07-08 12:23:07 -0500

    Add pelican config

---
+++ pelicanconf.py
@@ -0,0 +1,20 @@
+AUTHOR = 'Jennifer Wilcox'
+SITENAME = 'nitori.org'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Chicago'
+
+DEFAULT_LANG = 'en'
+
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+LINKS = None
+SOCIAL = None
+
+DEFAULT_PAGINATION = False
