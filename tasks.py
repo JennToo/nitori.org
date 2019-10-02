e1c6dde9b20dbfe1163ddb7e65186686824df072
Jennifer Wilcox <jennifer@nitori.org>
2019-10-02 12:46:18 -0500

    Try to deploy

--- tasks.py
+++ tasks.py
@@ -7,6 +7,11 @@ def serve(c):
     re_exec("pelican content -l -r -t theme -b 0.0.0.0")
 
 
+@task
+def build(c):
+    re_exec("pelican content -t theme")
+
+
 def re_exec(cmd):
     args = cmd.split(" ")
     os.execvp(args[0], args)
