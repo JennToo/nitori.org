b5c1c05b4b2dce154179dabd08d2b0544d887546
Jennifer Wilcox <jennifer@nitori.org>
2019-11-05 08:40:39 -0600

    Apparently it also ignores whitespace changes

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,12 +1,12 @@
-AUTHOR = 'Jennifer Wilcox'
-SITENAME = 'nitori.org'
-SITEURL = ''
+AUTHOR = "Jennifer Wilcox"
+SITENAME = "nitori.org"
+SITEURL = ""
 
-PATH = 'content'
+PATH = "content"
 
-TIMEZONE = 'America/Chicago'
+TIMEZONE = "America/Chicago"
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = "en"
 
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
